32015d1d22cd6c9426738bf7df084fd3b5f10324
conflictor <victor.maripangui@usach.cl>
2018-04-06 11:11:57 -0300

    Agregada lectura desde la consola con getopt

--- main.cpp
+++ main.cpp
@@ -1,12 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include<iostream>
 #include "imageControl.hpp"
 #include <string>
-int main(){
+using namespace std;
+int main(int argc, char **argv){
+
+    //DECLARACION DE VARIABLES PARA ARGUMENTOS RECIBIDOS
+    //c:cantidad de imágenes ; u: umbral; n: umbral para clasificacion; b: bandera(?)
+    int c,u,n,b;
+    int opt;
+    while((opt=getopt(argc, argv,"c:u:n:b:"))!=-1){
+        //SE ASIGNAN LOS VALORES DE LA CONSOLA A LAS VARIABLES DEFINIDAS
+        switch(opt){
+            case 'c':
+                c=atoi(optarg);
+                break;
+            case 'u':
+                u=atoi(optarg);
+                break;
+            case 'n':
+                n=atoi(optarg);
+                break;
+            case 'b':
+                b=atoi(optarg);
+                break;
+        }
+
+    }
+
+    //SE IMPRIMEN LOS VALORES RESCATADOS DESDE LA CONSOLA
+    cout << endl;
+    cout<<c<<" "<<u<<" "<<n<<" "<<b;
+
+
 	string str= "imagen.bmp";
 	char *cstr = &str[0u];
 
 
 	ImageControl imageControl=ImageControl();
 	imageControl.loadBMP(cstr);
-	
+
+
+
 	return 0;
 }
